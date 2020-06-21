f0cd209771e80c085915bf452a6d760e3d14a4fb
guorui <guorui6231@163.com>
2020-06-21 10:16:53 +0800

    启动服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-const greeting = 'river';
-console.log(greeting)
+const http=require('http')
+const server=http.createServer((request,response)=>{
+    response.write('helloe');
+    response.end();
+});
+
+server.listen(3000,()=>{
+    console.log("服务已经启动，重新出发！")
+})
