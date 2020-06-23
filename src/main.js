2d852be78f45563bf5400e4bbdd1121ecff8d4b9
guorui <guorui6231@163.com>
2020-06-24 05:52:05 +0800

    一次性提交更改

--- src/main.js
+++ src/main.js
@@ -1,12 +1,42 @@
 const http=require('http')
 const server=http.createServer((request,response)=>{
-    console.log(request.httpVersion)
+    response.writeHead(200,{
+        'Content-Type':"text/html;charset=utf-8"
+    })
 
+    const data={
+        book:"自由万岁",
+        price:"30",
+        id:1
+    } 
+    const jsondata=JSON.stringify(data)
+    switch(request.url){
+        case '/':
+            response.write("成功!",'utf-8');
+            break;
+        case '/zhuce':
+            response.write('打开注册界面');
+            break
+        case '/denglu':
+            response.write("打开登陆界面!");
+            break;
+        case '/shuru':
+            response.write('<b>我热爱自己的祖国</b>');
+            break;
+        case '/book':
+            response.writeHead(200,{
+                'Content-Type':"application/json;charset=utf-8"
+            })
+            response.write(jsondata);
+            break;
+        default:
+            response.writeHead(404);
+            response.write('404');
+            break;
+    }
 
-
-
-    response.write('helloe');
     response.end();
+
 });
 
 server.listen(3000,()=>{
